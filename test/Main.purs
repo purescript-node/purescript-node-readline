c8dbf964ded3fc0604e92ef5a8b1cc7bab5b1859
aspidites <emarshall85@gmail.com>
2015-08-03 18:13:21 -0400

    Flipped arguments for setLineHandler.

--- test/Main.purs
+++ test/Main.purs
@@ -10,11 +10,8 @@ import Node.ReadLine
 main = do
   interface <- createInterface noCompletion
   
-  let
-    lineHandler s = do
-      log $ "You typed: " ++ s
-      prompt interface
-  
   setPrompt "> " 2 interface
   prompt interface
-  setLineHandler lineHandler interface
+  setLineHandler interface $ \s -> do
+    log $ "You typed: " ++ s
+    prompt interface
