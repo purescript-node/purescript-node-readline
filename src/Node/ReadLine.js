cac5d9f4e25465650060da748995739fa2f38073
Phil Freeman <paf31@cantab.net>
2017-04-05 09:12:21 -0700

    Updates for 0.11 (#11)
    * Updates for 0.11
    
    * eslint
    
    * eslint --fix

--- src/Node/ReadLine.js
+++ src/Node/ReadLine.js
@@ -1,53 +1,53 @@
 /* global exports */
-"use strict";
+'use strict';
 
 // module Node.ReadLine
 
 exports.createInterfaceImpl = function (options) {
-  return function () {
-    var readline = require("readline");
-    return readline.createInterface({
-      input: options.input,
-      output: options.output,
-      completer: options.completer && function (line) {
-        var res = options.completer(line)();
-        return [res.completions, res.matched];
-      },
-      terminal: options.terminal,
-      historySize: options.historySize
-    });
-  };
+    return function () {
+        var readline = require('readline');
+        return readline.createInterface(
+            { input: options.input
+      , output: options.output
+      , completer: options.completer && function (line) {
+          var res = options.completer(line)();
+          return [res.completions, res.matched];
+      }
+      , terminal: options.terminal
+      , historySize: options.historySize
+            });
+    };
 };
 
 exports.close = function (readline) {
-  return function () {
-    readline.close();
-  };
+    return function () {
+        readline.close();
+    };
 };
 
 exports.prompt = function (readline) {
-  return function () {
-    readline.prompt();
-  };
+    return function () {
+        readline.prompt();
+    };
 };
 
 exports.setPrompt = function (prompt) {
-  return function (length) {
-    return function (readline) {
-      return function () {
-        readline.setPrompt(prompt, length);
-      };
+    return function (length) {
+        return function (readline) {
+            return function () {
+                readline.setPrompt(prompt, length);
+            };
+        };
     };
-  };
 };
 
 exports.setLineHandler = function (readline) {
-  return function (callback) {
-    return function () {
-      readline.removeAllListeners("line");
-      readline.on("line", function (line) {
-        callback(line)();
-      });
+    return function (callback) {
+        return function () {
+            readline.removeAllListeners('line');
+            readline.on('line', function (line) {
+                callback(line)();
+            });
+        };
     };
-  };
 };
