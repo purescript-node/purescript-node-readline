aa672d6a4d933957f056def1ae2812c0ae144b1b
Thomas Honeyman <admin@thomashoneyman.com>
2020-12-09 12:36:51 -0800

    Migrate to GitHub Actions (#19)

--- src/Node/ReadLine.js
+++ src/Node/ReadLine.js
@@ -1,65 +1,66 @@
-/* global exports */
-'use strict';
+"use strict";
 
 // module Node.ReadLine
 
 exports.createInterfaceImpl = function (options) {
-    return function () {
-        var readline = require('readline');
-        return readline.createInterface(
-            { input: options.input
-      , output: options.output
-      , completer: options.completer && function (line) {
+  return function () {
+    var readline = require("readline");
+    return readline.createInterface({
+      input: options.input,
+      output: options.output,
+      completer:
+        options.completer &&
+        function (line) {
           var res = options.completer(line)();
           return [res.completions, res.matched];
-      }
-      , terminal: options.terminal
-      , historySize: options.historySize
-            });
-    };
+        },
+      terminal: options.terminal,
+      historySize: options.historySize,
+    });
+  };
 };
 
 exports.close = function (readline) {
-    return function () {
-        readline.close();
-    };
+  return function () {
+    readline.close();
+  };
 };
 
 exports.prompt = function (readline) {
-    return function () {
-        readline.prompt();
-    };
+  return function () {
+    readline.prompt();
+  };
 };
 
-exports.question = function(text) {
-    return function(callback) {
-        return function(readline) {
-            return function() {
-                readline.question(text, function(result) {
-                    callback(result)();
-                });
-            };
-        };
+exports.question = function (text) {
+  return function (callback) {
+    return function (readline) {
+      return function () {
+        readline.question(text, function (result) {
+          callback(result)();
+        });
+      };
     };
+  };
 };
 
 exports.setPrompt = function (prompt) {
-    return function (length) {
-        return function (readline) {
-            return function () {
-                readline.setPrompt(prompt, length);
-            };
-        };
+  return function (length) {
+    return function (readline) {
+      return function () {
+        readline.setPrompt(prompt, length);
+      };
     };
+  };
 };
 
 exports.setLineHandler = function (readline) {
-    return function (callback) {
-        return function () {
-            readline.removeAllListeners('line');
-            readline.on('line', function (line) {
-                callback(line)();
-            });
-        };
+  return function (callback) {
+    return function () {
+      readline.removeAllListeners("line");
+      readline.on("line", function (line) {
+        callback(line)();
+      });
     };
+  };
 };
