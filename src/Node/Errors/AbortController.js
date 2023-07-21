faa0cede4406f827fc7a08229a99fe24eb04daf5
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-07-21 16:04:03 -0700

    Fix FFI

--- src/Node/Errors/AbortController.js
+++ src/Node/Errors/AbortController.js
@@ -2,4 +2,4 @@ const newImpl = () => new AbortController();
 export { newImpl as new };
 export const abortImpl = (controller) => controller.abort();
 export const abortReasonImpl = (controller, reason) => controller.abort(reason);
-export const signal = (controller) => controller.signal(reason);
+export const signal = (controller) => controller.signal;
